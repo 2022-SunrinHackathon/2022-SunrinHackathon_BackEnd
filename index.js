f44eb2ec72aaad2ee5e957fbcf0c4354cd575485
iamraram <slyram06@icloud.com>
2022-07-15 19:46:36 +0900

    add:signup

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const passport = require('passport')
-const dotenv = require('dotenv')
-const session = require('express-session')
-const axios = require("axios")
-const router = express.Router()
-const MongoClient = require('mongodb').MongoClient
+export const express = require('express')
+export const passport = require('passport')
+export const dotenv = require('dotenv')
+export const session = require('express-session')
+export const axios = require("axios")
+export const router = express.Router()
+export const MongoClient = require('mongodb').MongoClient
 
-const app = express()
+export const app = express()
 dotenv.config()
 
 app.use(express.static('public'))
@@ -16,10 +16,10 @@ app.use(passport.session(undefined))
 
 app.use(express.urlencoded({extended: true}))
 
-var db_user, db_list, db_api
-var id_db = process.env.ID
-var password_db = process.env.PASSWORD
-var backurl = '@cluster0.fzore.mongodb.net/?retryWrites=true&w=majority'
+export let db_user, db_list, db_api
+export const id_db = process.env.ID
+export const password_db = process.env.PASSWORD
+export const backurl = '@cluster0.fzore.mongodb.net/?retryWrites=true&w=majority'
 
 MongoClient.connect('mongodb+srv://' + id_db + ':' + password_db + backurl,
     function(err, client) {
@@ -31,39 +31,52 @@ MongoClient.connect('mongodb+srv://' + id_db + ':' + password_db + backurl,
 
     app.listen(8080, function (req, res) {
         console.log('8080에 연결함')
-        db_user.collection('user').insertOne({
-            id: 'haram',
-            pw: '12345678',
-            name: '억울한라람',
-            live_region: {
-                house: '응암동',
-                school: '청파동',
-                most_visit: [
-                    '역촌동',
-                    '중산동',
-                    '마두동'
-                ]
-            }
-        },
-        function (err, result) {
-            if (err) {
-                console.log('err')
-            }
-            else {
-                console.log('success')
-            }
-        })
     });
 
     app.get('/signup', function(req, res){
-        db_user.collection('user').findOne({ name: '억울한라람' },
-            function(err, result) {
-                console.log(result)
-                res.json({
-                    result
+        const email_value = 'iamraram'
+        const pw_value = '123456'
+        const pw_value2 = '123456'
+        const name_value = '이하람'
+
+        function signup(data) {
+            db_user.collection('user').insertOne(data,
+                function (err,result) {
+                    if (err) {
+                        console.log('error occurred')
+                    }
+                    else {
+                        console.log('success to signup someone user')
+                    }
                 })
+        }
+
+        if (email_value === '^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$') {
+            if (pw_value.length >= 8) {
+                if (name_value === '/^[가-힣\\s]+$/') {
+                    if (pw_value === pw_value2) {
+                        const user_info = {
+                            user_email: email_value,
+                            user_pw: pw_value,
+                            user_name: name_value
+                        }
+                        signup(user_info)
+                    }
+                    else {
+                        res.end('incorrect password and checking password')
+                    }
+                }
+                else {
+                    res.end('incorrect name value')
+                }
             }
-        );
+            else {
+                res.end('incorrect password length')
+            }
+        }
+        else {
+            res.end('incorrect email address')
+        }
     });
 
 })
